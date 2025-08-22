7f136dc1d077518106a21e4f0ceee20963c8f41c
DESKTOP-25S55MP\kss78 <kss7858@naver.com>
2025-08-23 00:46:24 +0900

    test3
    Signed-off-by: DESKTOP-25S55MP\kss78 <kss7858@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,28 @@ function App() {
 
   useEffect(() => {
     // 핀치 줌 방지
-    const handleTouchMove = (e: TouchEvent) => {
-      if (e.touches.length > 1 || (e as any).scale !== 1) {
-        e.preventDefault();
-      }
-    };
-
-    // 더블탭 줌 방지
-    // let lastTouchEnd = 0;
-    // const handleTouchEnd = (e: TouchEvent) => {
-    //   const now = Date.now();
-    //   if (now - lastTouchEnd <= 300) {
+    // const handleTouchMove = (e: TouchEvent) => {
+    //   if (e.touches.length > 1 || (e as any).scale !== 1) {
     //     e.preventDefault();
     //   }
-    //   lastTouchEnd = now;
     // };
 
-    document.addEventListener("touchmove", handleTouchMove, { passive: false });
-    //document.addEventListener("touchend", handleTouchEnd, false);
+    // 더블탭 줌 방지
+    let lastTouchEnd = 0;
+    const handleTouchEnd = (e: TouchEvent) => {
+      const now = Date.now();
+      if (now - lastTouchEnd <= 300) {
+        e.preventDefault();
+      }
+      lastTouchEnd = now;
+    };
+
+    // document.addEventListener("touchmove", handleTouchMove, { passive: false });
+    document.addEventListener("touchend", handleTouchEnd, false);
 
     return () => {
-      document.removeEventListener("touchmove", handleTouchMove);
-      //document.removeEventListener("touchend", handleTouchEnd);
+      // document.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("touchend", handleTouchEnd);
     };
   }, []);
 
